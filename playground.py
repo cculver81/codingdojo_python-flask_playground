c12135b3e37a9d360aa38e397b9b431469645dfa
Cinque Culver <culver.ce@gmail.com>
2020-04-13 12:22:23 -0400

    Project complete excepted for centering divs/colored squares

---
+++ playground.py
@@ -0,0 +1,27 @@
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/play')
+def play():
+    inputRoute='landing'
+    return render_template('index.html', inputRoute=inputRoute)
+
+@app.route('/play/<int:times>')
+def play_times(times):
+    inputRoute='times'
+    return render_template('index.html', inputRoute=inputRoute, times=times)
+
+
+@app.route('/play/<int:times>/<string:color>')
+def play_times_color(times, color):
+    inputRoute='color'
+    # style = f'<div class="square" style="background-color: {color};"></div>'
+    # style = f'background-color: {color});'
+    # print('*'*10 + style)
+    return render_template('index.html', inputRoute=inputRoute, times=times, color=color)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
