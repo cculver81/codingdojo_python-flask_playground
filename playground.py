f05506f58d899dff36dcfaa5f6da5044aea8905d
Cinque Culver <culver.ce@gmail.com>
2020-04-13 14:01:51 -0400

    Got the divs/colored squares centered

--- playground.py
+++ playground.py
@@ -6,12 +6,14 @@ app = Flask(__name__)
 @app.route('/play')
 def play():
     inputRoute='landing'
-    return render_template('index.html', inputRoute=inputRoute)
+    # return render_template('index.html')
+    return render_template('index.html', inputRoute=inputRoute) #Bonus: Implement all routes with one template
 
 @app.route('/play/<int:times>')
 def play_times(times):
     inputRoute='times'
-    return render_template('index.html', inputRoute=inputRoute, times=times)
+    # return render_template('index.html', times=times)
+    return render_template('index.html', inputRoute=inputRoute, times=times) #Bonus: Implement all routes with one template
 
 
 @app.route('/play/<int:times>/<string:color>')
@@ -20,7 +22,8 @@ def play_times_color(times, color):
     # style = f'<div class="square" style="background-color: {color};"></div>'
     # style = f'background-color: {color});'
     # print('*'*10 + style)
-    return render_template('index.html', inputRoute=inputRoute, times=times, color=color)
+    # return render_template('index.html', times=times, color=color)
+    return render_template('index.html', inputRoute=inputRoute, times=times, color=color) #Bonus: Implement all routes with one template
 
 
 if __name__ == '__main__':
